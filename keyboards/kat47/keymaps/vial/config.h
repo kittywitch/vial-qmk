1ffa853f85d0b648c6db311b13421733f7f15e27
Kat Inskip <kat@inskip.me>
2025-02-23 13:29:39 -0800

    add kat47, meep

---
+++ keyboards/kat47/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x05, 0x93, 0x65, 0x55, 0x12, 0x16, 0x3E, 0x84}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
